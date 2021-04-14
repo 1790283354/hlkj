d553e32ad70084827c9715ef6578063e7684ae20
1790283354 <79694002+1790283354@users.noreply.github.com>
2021-04-14 10:36:32 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<linkrel="shortcut icon" href="assets/img/bg/favicon.ico"><linkrel="Bookmark" href="assets/img/bg/favicon.ico">
+		<title>沃韦特官网</title>
+		<meta name="description" content="沃德韦尔特科研团（WonderWave Research Team，原徕柯科研）,成立于2018年7月19日。目前业务主要致力于设计和开发智能穿戴设备软件，作品主要集中于浪坛社区（wonderwavert.com）发布。我们由一群有梦想的年轻人组成，未来还将把业务拓展到MIUI主题设计、MC服务器、数码圈论坛、LOGO设计、国外视频汉化等领域，敬请拭目以待。" />
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<meta name="format-detection" content="telephone=no">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<!-- Fonts-->
+		<link rel="stylesheet" type="text/css" href="assets/fonts/fontawesome/font-awesome.min.css">
+		<!-- Vendors-->
+		<link rel="stylesheet" type="text/css" href="assets/vendors/bootstrap/grid.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendors/magnific-popup/magnific-popup.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendors/swiper/swiper.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendors/wow/animate.css">
+		<!-- App & fonts-->
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700|Montserrat:400,700">
+		<link rel="stylesheet" type="text/css" id="app-stylesheet" href="assets/css/main.css"><!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<![endif]-->
+
+	</head>
+	
+	<body>
+		<div class="page-wrap" id="root">
+			
+			<!-- header -->
+			<header class="header header--fixed">
+				<div class="container">
+					<div class="header__inner">
+						<div class="header__logo"><a href="./">沃德韦尔特科研团</a></div>
+						<div class="header__menu">
+							
+							<!-- onepage-nav -->
+							<nav class="onepage-nav">
+								
+								<!-- onepage-menu -->
+								<ul class="onepage-menu">
+									<li class="current-menu-item"><a href="#id-1">主页</a>
+									</li>
+									<li><a href="http://wonderwavert.com/">服务</a>
+									</li>
+									<li><a href="http://wonderwavert.com/">项目</a>
+									</li>
+									<li><a href="#id-2">关于</a>
+									</li>
+									<li><a href="https://qm.qq.com/cgi-bin/qm/qr?k=Vwzl_vqmrD0Ijnz50AJdtEJz0crtDmwy&jump_from=webapi">群组</a>
+									</li>
+								</ul><!-- onepage-menu -->
+								
+								<div class="navbar-toggle"><span></span><span></span><span></span></div>
+							</nav><!-- End / onepage-nav -->
+							
+						</div>
+					</div>
+				</div>
+			</header><!-- End / header -->
+			
+			<!-- Content-->
+			<div class="awe-content">
+				
+				<!-- hero -->
+				<section class="hero awe-text-center" id="id-1">
+					<div class="awe-overlay"></div>
+					<div class="hero__wrapper">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-12 col-lg-8 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 col-lg-offset-2 ">
+									<h1 class="hero__title">沃德韦尔特科研团</h1>
+									<p class="hero__text">WonderWavert.</p>
+									<a class="a-btn btn-first" href="http://wonderwavert.com/thread-24-1-1.html">申请加入</a><a class="a-btn btn-second" href="http://wonderwavert.com">前往社区</a><a class="a-btn btn-second" href="/zm/">招贤纳士</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					<!-- smoky -->
+					<div class="smoky" id="smoky-bg">
+						<canvas>Your browser does not support HTML5 canvas.</canvas>
+					</div><!-- End / smoky -->
+					
+				</section><!-- End / hero -->
+				
+				
+				<!-- Section -->
+				<section class="awe-section awe-text-center paragraph bg-gray">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-8 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 col-lg-offset-2 "> 
+								<p class="fz-22">沃德韦尔特科研团</br>WonderWave Research Team.
+								</p>
+							</div>
+						</div>
+					</div>
+				</section>
+				<!-- End / Section -->
+				
+				
+				<!-- Section -->
+				<section class="awe-section" id="id-2">
+					<div class="container">
+						<div class="row">
+							
+							<!-- title -->
+							<div class="title">
+								<h2 class="title__title">关于我们
+									<p class="title__text">About us.</p>
+								</h2>
+							</div><!-- End / title -->
+							
+							
+							<!-- about -->
+							<div class="about">
+								<div class="col-lg-6 ">
+									<div class="about__words">
+										<h2 class="fw-600 fz-30">我们的介绍</h2>
+										<p>沃德韦尔特科研团（WonderWave Research Team，原徕柯科研）,成立于2018年7月19日。目前业务主要致力于设计和开发智能穿戴设备软件，作品主要集中于浪坛社区（wonderwavert.com）发布。我们由一群有梦想的年轻人组成，未来还将把业务拓展到MIUI主题设计、MC服务器、数码圈论坛、LOGO设计、国外视频汉化等领域，敬请拭目以待。</p>
+										<p>如果您想加入我们，<a class="a-btn" href="http://wonderwavert.com/thread-24-1-1.html">请点击此处</a>感谢您的支持。</p><a class="a-btn" href="http://wonderwavert.com/">跳转到浪坛社区</a>
+									</div>
+								</div>
+								<div class="col-lg-6 ">
+									<div class="about__img"><img src="http://wonderwavert.com/td.jpg"/></div>
+								</div>
+							</div><!-- End / about -->
+							
+						</div>
+					</div>
+				</section>
+				<!-- End / Section -->
+				
+				
+				<!-- Section -->
+				<section class="awe-section bg-gray">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-6 ">
+								
+								<!-- progress -->
+								<div class="progress">
+									<h5 class="progress__title">设计师</h5>
+									<div class="progress__wrap">
+										<div class="progress__bar" data-progress-percent="92" data-timing="ease" data-duration="1000" data-delay="500"><span class="progress__number">92%</span></div>
+									</div>
+								</div><!-- End / progress -->
+								
+								
+								<!-- progress -->
+								<div class="progress">
+									<h5 class="progress__title">程序员</h5>
+									<div class="progress__wrap">
+										<div class="progress__bar" data-progress-percent="84" data-timing="ease" data-duration="1000" data-delay="500"><span class="progress__number">84%</span></div>
+									</div>
+								</div><!-- End / progress -->
+								
+							</div>
+							<div class="col-lg-6 ">
+								
+								<!-- progress -->
+								<div class="progress">
+									<h5 class="progress__title">手环开发者</h5>
+									<div class="progress__wrap">
+										<div class="progress__bar" data-progress-percent="90" data-timing="ease" data-duration="1000" data-delay="500"><span class="progress__number">90%</span></div>
+									</div>
+								</div><!-- End / progress -->
+								
+								
+								<!-- progress -->
+								<div class="progress">
+									<h5 class="progress__title">软件开发者</h5>
+									<div class="progress__wrap">
+										<div class="progress__bar" data-progress-percent="84" data-timing="ease" data-duration="1000" data-delay="500"><span class="progress__number">84%</span></div>
+									</div>
+								</div><!-- End / progress -->
+								
+							</div>
+						</div>
+					</div>
+				</section>
+				<!-- End / Section -->
+				
+				<div class="tlinks">Collect from <a href="#" >WonderWavert</a></div>
+								
+				
+				<!-- Section -->
+				<section class="awe-section awe-text-center" id="id-5">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12">
+								
+								<!-- title -->
+								<div class="title">
+									<h2 class="title__title">成员列表
+									</h2>
+								</div><!-- End / title -->
+								
+							</div>
+						</div>
+						<div class="row">
+							
+							<!-- swiper__module swiper-container -->
+							<div class="swiper__module swiper-container" data-options='{"slidesPerView":3,"slidesPerColumn":1,"breakpoints":{"320":{"slidesPerView":1,"spaceBetween":10},"640":{"slidesPerView":1,"spaceBetween":20},"768":{"slidesPerView":2,"spaceBetween":30},"1024":{"slidesPerView":3,"spaceBetween":40}}}'>
+								<div class="swiper-wrapper">
+<?php
+include './conn.php';
+$sql = "SELECT * FROM user where see = 1";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // 输出数据
+    while($row = mysqli_fetch_assoc($result)) {
+        echo '<div class="post"><div class="post__media"><a href="#"><img src="http://q1.qlogo.cn/g?b=qq&amp;nk='.$row['qq'].'&amp;s=640"/></a></div><div><h2 class="post__title"><a href="#">'.$row['name'].'</a></h2><p class="post__text">'.$row['nr'].'</div></div>';
+      }
+    }else {
+    echo "0 结果";
+}
+ 
+mysqli_close($conn);
+
+?>										
+								</div>
+								<div class="swiper-pagination-custom"></div>
+							</div><!-- End / swiper__module swiper-container -->
+							
+						</div>
+					</div>
+				</section>
+				<!-- End / Section -->
+				
+				
+				<!-- Section -->
+				<section class="awe-section quote" style="background-image:url('assets/img/bg/testimonial.jpg');">
+					<div class="awe-overlay" style="background-color:#000;opacity:0.75;"></div>
+					<div class="container">
+						
+						<!-- title -->
+						<div class="title">
+							<h2 class="title__title"><span class='first'>QQ交流群</span><br />938432832
+								<p class="title__text"></p>
+							</h2>
+						</div><!-- End / title -->
+						
+			<!-- footer -->
+			<div class="footer">
+				<div id="back-to-top"><i class="fa fa-angle-up"></i></div>
+				<div class="container">
+					<div class="footer__wrapper">
+						<p class="footer__copy">2018-2021 &copy; Copyright <a href="#">WonderWave Research Team</a>沃德韦尔特科研团<a href="https://www.miit.gov.cn/" target="_blank" title="WonderWave Research Team">冀ICP备2021002684号</a></p>
+					</div>
+				</div>
+			</div><!-- End / footer -->
+			
+		</div>
+		<!-- Vendors-->
+		<script type="text/javascript" src="assets/vendors/_jquery/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/imagesloaded/imagesloaded.pkgd.js"></script>
+		<script type="text/javascript" src="assets/vendors/isotope-layout/isotope.pkgd.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery-one-page/jquery.nav.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery.easing/jquery.easing.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery.matchHeight/jquery.matchHeight.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/masonry-layout/masonry.pkgd.js"></script>
+		<script type="text/javascript" src="assets/vendors/swiper/swiper.jquery.js"></script>
+		<script type="text/javascript" src="assets/vendors/menu/menu.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery.countTo/jquery.countTo.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery.waypoints/jquery.waypoints.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/tabs/awe-tabs.js"></script>
+		<script type="text/javascript" src="assets/vendors/wow/wow.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery.appear/jquery.appear.js"></script>
+		<script type="text/javascript" src="assets/vendors/waterpipe/waterpipe.js"></script>
+		<!-- App-->
+		<script type="text/javascript" src="assets/js/main.js"></script>
+	</body>
+</html>
